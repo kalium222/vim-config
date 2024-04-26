16aa36c44e2c02f00e4bb3cf72c7dee5ebe98945
kalium <kalium222@outlook.com>
2024-04-27 04:09:38 +0800

    feat: cursor shape

--- lua/options.lua
+++ lua/options.lua
@@ -8,6 +8,8 @@ vim.opt.wrap = false
 vim.opt.showcmd = true
 vim.opt.showmode = true
 vim.opt.ruler = true
+-- vim.g.guicursor = 'n-v-c-sm:block,i-ci-ve:hor25,r-cr-o:ver20'
+vim.cmd[[set guicursor=n-v-c-sm:block,i-ci-ve:hor20,r-cr-o:hor50]]
 
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,3 +8,4 @@ local colorscheme = {
 }
 
 return colorscheme
+
