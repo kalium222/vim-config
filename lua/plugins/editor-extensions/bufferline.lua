4da508839e398b4967affc6e32197e16e56b0113
kalium <kalium222@outlook.com>
2024-04-27 05:40:59 +0800

    feat: bufferline

---
+++ lua/plugins/editor-extensions/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+    'akinsho/bufferline.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('bufferline').setup {
+            options = {
+                mode = "tabs",
+                indicator = {
+                    style = "underline"
+                },
+            },
+        }
+    end,
+}
