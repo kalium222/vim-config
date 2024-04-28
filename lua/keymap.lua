380c2d50cec8f7265a22e70f74885a9c298ace72
kalium <kalium222@outlook.com>
2024-04-28 14:44:43 +0800

    feat: telescope, tweaks

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -21,3 +21,9 @@ vim.keymap.set('n', '<Leader>|', '<cmd>NvimTreeFocus<CR>', opts)
 
 vim.keymap.set('n', '<Leader>m', '<cmd>MarkdownPreviewToggle<CR>', opts)
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
---
+++ lua/plugins/editor-extensions/telescope.lua
@@ -0,0 +1,8 @@
+local telescope = {
+    'nvim-telescope/telescope.nvim',
+    event = "VeryLazy",
+    dependencies = { 'nvim-lua/plenary.nvim' }
+}
+
+return telescope
+
