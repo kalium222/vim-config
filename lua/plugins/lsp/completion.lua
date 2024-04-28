0cd4a6e32952a3330f77e226752203edd5c07a63
kalium <kalium222@outlook.com>
2024-04-28 12:30:49 +0800

    feat: nvim-cmp keymap

--- lua/plugins/lsp/completion.lua
+++ lua/plugins/lsp/completion.lua
@@ -20,10 +20,6 @@ local completion = {
                 -- REQUIRED - you must specify a snippet engine
                 expand = function(args)
                     vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                    -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-                    -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
                 end,
             },
             window = {
@@ -31,6 +27,8 @@ local completion = {
                 documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
+                ['<Tab>'] = cmp.mapping.select_next_item(),
+                ['<S-Tab>'] = cmp.mapping.select_prev_item(),
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete(),
