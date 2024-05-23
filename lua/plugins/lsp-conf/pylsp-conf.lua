9bff6bbd5f2a93c77386ef26bd84b17f6607904c
kalium <kalium222@outlook.com>
2024-05-23 10:44:02 +0800

    feat: change to pylsp for python

---
+++ lua/plugins/lsp-conf/pylsp-conf.lua
@@ -0,0 +1,17 @@
+-- pylsp
+require'lspconfig'.pylsp.setup{
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {'W391'},
+          maxLineLength = 100
+        }
+      }
+    }
+  }
+}
+
+return {}
+
