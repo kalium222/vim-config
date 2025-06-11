2e25f03c05c703487017f0a5a0fbd6c52a2c217b
kalium <kalium222@outlook.com>
2025-06-11 16:14:31 +0800

    hls config; fixed csharp-ls config

--- lua/plugins/lsp-conf/csharp-ls-conf.lua
+++ lua/plugins/lsp-conf/csharp-ls-conf.lua
@@ -2,7 +2,11 @@
 
 vim.lsp.enable('csharp_ls')
 vim.lsp.config('csharp_ls', {
-    cmd = {"/home/Kalium/.dotnet/tools/csharp-ls"},
+  cmd = {"/home/Kalium/.dotnet/tools/csharp-ls"},
+  filetypes = {"cs"},
+  init_options = {
+    AutomaticWorkspaceInit = true,
+  },
 })
 
 return {}
--- lua/plugins/lsp-conf/hls-conf.lua
+++ lua/plugins/lsp-conf/hls-conf.lua
@@ -3,6 +3,13 @@
 vim.lsp.enable('hls')
 vim.lsp.config('hls', {
   filetypes = { 'haskell', 'lhaskell', 'cabal' },
+  cmd = { "haskell-language-server-wrapper", "--lsp" },
+  settings = {
+    haskell = {
+      cabalFormattingProvider = "cabalfmt",
+      formattingProvider = "ormolu"
+    }
+  },
 })
 
 return {}
