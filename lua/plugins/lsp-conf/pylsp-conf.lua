1f1db0993e05a8f5813d39941e43710e72bb65fd
kalium <kalium222@outlook.com>
2025-06-20 13:00:00 +0800

    update: config for pylsp

--- lua/plugins/lsp-conf/pylsp-conf.lua
+++ lua/plugins/lsp-conf/pylsp-conf.lua
@@ -6,7 +6,12 @@ vim.lsp.config('pylsp', {
     pylsp = {
       plugins = {
         pycodestyle = {
-          ignore = {'W391', 'W191', 'E302'},
+          ignore = {'W391',
+            'W191',
+            'E302',
+            'E402', -- import not at top of the file
+            'E501', -- longer than 100 characters
+          },
           maxLineLength = 100
         }
       }
