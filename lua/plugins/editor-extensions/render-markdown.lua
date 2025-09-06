4ac62a9ffe36ccffdcd006d259f1b33ab2b82cda
kalium <kalium222@outlook.com>
2025-09-06 19:21:12 -0400

    update: quit markdown-preview.nvim for md. use render-markdown.nvim

---
+++ lua/plugins/editor-extensions/render-markdown.lua
@@ -0,0 +1,11 @@
+local render_markdown = {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+}
+
+return render_markdown
