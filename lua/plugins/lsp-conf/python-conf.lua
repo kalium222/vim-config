84578789c860e327c40da344618d3720b63e74e5
kalium <kalium222@outlook.com>
2025-09-20 00:05:16 -0400

    fix: disable linter of pyright

--- lua/plugins/lsp-conf/python-conf.lua
+++ lua/plugins/lsp-conf/python-conf.lua
@@ -1,4 +1,23 @@
 -- pyright and ruff
 vim.lsp.enable('ruff')
+vim.lsp.config('pyright', {
+    capabilities = (function()
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+        return capabilities
+    end)(),
+    settings = {
+        python = {
+            analysis = {
+                useLibraryCodeForTypes = true,
+                diagnosticSeverityOverrides = {
+                    reportUnusedVariable = "warning",
+                },
+                typeCheckingMode = "off", -- Set type-checking mode to off
+                diagnosticMode = "off", -- Disable diagnostics entirely
+            },
+        },
+    },
+})
 vim.lsp.enable('pyright')
 return {}
