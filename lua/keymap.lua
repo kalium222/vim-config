22c67da482b5143d7cf53d958df2d5f9d87dfcc4
kalium <kalium222@outlook.com>
2026-03-01 22:49:33 -0500

    tweaks

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -15,38 +15,26 @@ local mappings = {
     { mode = 'n', lhs = '<Leader>k',  rhs = [[<C-w>k | :wall<CR>]],                     opts = opts },
     { mode = 'n', lhs = '<Leader>l',  rhs = [[<C-w>l | :wall<CR>]],                     opts = opts },
     -- tab page jump
-    { mode = 'n', lhs = '<Leader>-',  rhs = '<cmd>tabp<CR> | <cmd>wall<cr>',            opts = opts },
-    { mode = 'n', lhs = '<Leader>=',  rhs = '<cmd>tabn<CR> | <cmd>wall<cr>',            opts = opts },
+    { mode = 'n', lhs = '<Leader>-',  rhs = '<cmd>tabp<CR> | <cmd>wall<CR>',            opts = opts },
+    { mode = 'n', lhs = '<Leader>=',  rhs = '<cmd>tabn<CR> | <cmd>wall<CR>',            opts = opts },
     -- nvimtree
     { mode = 'n', lhs = '<Leader>\\', rhs = '<cmd>NvimTreeToggle<CR>',                  opts = opts },
     { mode = 'n', lhs = '<Leader>|',  rhs = '<cmd>NvimTreeFocus<CR>',                   opts = opts },
-    -- markdown preview
-    { mode = 'n', lhs = '<Leader>m',  rhs = '<cmd>RenderMarkdown buf_toggle<CR>',       opts = opts },
     -- Lspsaga
     -- definition
-    { mode = 'n', lhs = '<Leader>gd', rhs = '<cmd>Lspsaga goto_definition<CR>',         opts = opts },
-    { mode = 'n', lhs = '<Leader>pd', rhs = '<cmd>Lspsaga peek_definition<CR>',         opts = opts },
-    -- type definition
-    { mode = 'n', lhs = '<Leader>gt', rhs = '<cmd>Lspsaga goto_type_definition<CR>',    opts = opts },
-    { mode = 'n', lhs = '<Leader>pt', rhs = '<cmd>Lspsaga peek_type_definition<CR>',    opts = opts },
-    -- goto other places: declaration, implementation
-    { mode = 'n', lhs = '<Leader>gD', rhs = vim.lsp.buf.declaration,                    opts = opts },
-    { mode = 'n', lhs = '<Leader>gi', rhs = vim.lsp.buf.implementation,                 opts = opts },
-    -- lsp format
-    { mode = 'n', lhs = '<Leader>F',  rhs = vim.lsp.buf.format,                         opts = opts },
+    { mode = 'n', lhs = 'gd',         rhs = '<cmd>Lspsaga goto_definition<CR>',         opts = opts },
+    { mode = 'n', lhs = 'gt',         rhs = '<cmd>Lspsaga goto_type_definition<CR>',    opts = opts },
     -- references
-    { mode = 'n', lhs = '<Leader>gr', rhs = '<cmd>Lspsaga finder<CR>' },
+    { mode = 'n', lhs = 'gr',         rhs = '<cmd>Lspsaga finder<CR>',                  opts = opts },
+    -- lsp format
+    { mode = 'n', lhs = '<Leader>F',          rhs = vim.lsp.buf.format,                         opts = opts },
     -- rename
     { mode = 'n', lhs = '<Leader>rn', rhs = '<cmd>Lspsaga rename<CR>',                  opts = opts },
     -- hover
-    { mode = 'n', lhs = '<Leader>K',  rhs = '<cmd>Lspsaga hover_doc<CR>' },
+    { mode = 'n', lhs = 'K',          rhs = '<cmd>Lspsaga hover_doc<CR>',               opts = opts },
     -- diagnostic info
-    { mode = 'n', lhs = '<Leader>g[', rhs = '<cmd>Lspsaga diagnostic_jump_prev<CR>' },
-    { mode = 'n', lhs = '<Leader>g]', rhs = '<cmd>Lspsaga diagnostic_jump_next<CR>' },
-    -- code action
-    { mode = 'n', lhs = '<Leader>pa', rhs = '<cmd>Lspsaga code_action<CR>' },
-    -- Lspsaga float terminal
-    -- { mode = 'n', lhs = '<Leader>t',  rhs = [[:wall<CR> | :Lspsaga term_toggle<CR>]] },
+    { mode = 'n', lhs = '<Leader>d',  rhs = '<cmd>Lspsaga show_line_diagnostics<CR>',   opts = opts },
+    { mode = 'n', lhs = '<Leader>D',  rhs = '<cmd>Lspsaga show_buf_diagnostics<CR>',    opts = opts },
     -- Telescope
     { mode = 'n', lhs = '<Leader>ff', rhs = '<cmd>Telescope find_files<CR>',            opts = opts },
     { mode = 'n', lhs = '<Leader>fg', rhs = '<cmd>Telescope live_grep<CR>',             opts = opts },
