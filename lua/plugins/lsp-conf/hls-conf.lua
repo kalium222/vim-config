0d5fa10518a921bb247858f58f8c7f5cf82b3884
kalium <kalium222@outlook.com>
2026-06-08 22:09:36 -0400

    tweaks

--- lua/plugins/lsp-conf/hls-conf.lua
+++ lua/plugins/lsp-conf/hls-conf.lua
@@ -1,16 +1,3 @@
 -- haskell language server
-vim.lsp.config('hls', {
-  filetypes = { 'haskell', 'lhaskell', 'cabal' },
-  cmd = { "haskell-language-server-wrapper", "--lsp" },
-  settings = {
-    haskell = {
-      cabalFormattingProvider = "cabalfmt",
-      formattingProvider = "ormolu",
-      plugin = {
-        rename = { config = { crossModule = true } }
-      },
-    },
-  },
-})
 vim.lsp.enable('hls')
 return {}
